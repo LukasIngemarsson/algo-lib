22a0f0011efbd8f1caad0deed16c2212b700c9c7
lukin921 <lukin921@student.liu.se>
2025-05-22 18:11:56 +0200

    added final algos from TDDD95 + renamed files

---
+++ point.cpp
@@ -0,0 +1,96 @@
+// Author: Lukas Ingemarsson
+#include <bits/stdc++.h>
+using namespace std;
+
+/* 2D point class. Defaults to (0, 0) if x and y are not provided. */
+template<typename T>
+struct Point {
+    T x, y;
+
+    Point (T x = 0, T y = 0) : x(x), y(y) {}
+
+    /* Addition. */
+    Point operator+(const Point& other) const {
+        return Point(x + other.x, y + other.y);
+    }
+
+    /* Subtraction. */
+    Point operator-(const Point& other) const {
+        return Point(x - other.x, y - other.y);
+    }
+
+    /* Multiplication (w/ point on left side). */
+    Point operator*(T scalar) const {
+        return Point(x * scalar, y * scalar);
+    }
+
+    /* Multiplication (w/ point on right side). */
+    friend Point operator*(T scalar, const Point& p) {
+        return Point(p.x * scalar, p.y * scalar);
+    }
+
+    /* Division. */
+    Point operator/(T scalar) const {
+        return Point(x / scalar, y / scalar);
+    }
+
+    /* Dot product. */
+    T dot(const Point& other) const {
+        return x * other.x + y * other.y;
+    }
+
+    /* Cross product. */
+    T cross(const Point& other) const {
+        return x * other.y - y * other.x;
+    }
+
+    /* Norm. */
+    double norm() const {
+        return sqrt(x * x + y * y);
+    }
+
+    /* Distance to another point. */
+    double dist_to(const Point& other) const {
+        return (*this - other).norm();
+    }
+
+    /* Angle with another point (in radians). */
+    double angle_with(const Point& other) const {
+        double dot_prod = this->dot(other);
+        double norm_prod = this->norm() * other.norm();
+        return acos(dot_prod / norm_prod);
+    }
+
+    /* Angle with origin (in radians). */
+    double angle() const {
+        return atan2(y, x);
+    }
+
+    bool operator<(const Point& other) const {
+        if (x == other.x) return y < other.y;
+        return x < other.x;
+    }
+
+    bool operator>(const Point& other) const {
+        if (x == other.x) return y > other.y;
+        return x > other.x;
+    }
+
+    bool operator==(const Point& other) const {
+        return x == other.x && y == other.y;
+    }
+
+    bool operator!=(const Point& other) const {
+        return x != other.x || y != other.y;
+    }
+
+    friend istream& operator>>(std::istream& is, Point& p) {
+        is >> p.x >> p.y;
+        return is;
+    }
+
+    friend ostream& operator<<(std::ostream& os, const Point& p) {
+        os << '(' << p.x << ", " << p.y << ')';
+        return os;
+    }
+};
