b9fe6e6e0c4fc1dbf48ce70da512325bfb4bb9d7
lukin921 <lukin921@student.liu.se>
2025-10-18 23:28:30 +0200

    separated fast io from default python template

--- templates/template.py
+++ templates/template.py
@@ -1,19 +1,7 @@
-import sys
-# import io, os
 
-def fast_io():
-    input = sys.stdin.readline
-
-    # # reads bytes
-    # # use input().decode().strip() for strings, otherwise just cast
-    # input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
-
-    print = sys.stdout.write # can only output strings
-
-    return input, print
 
 def main():
-    input, print = fast_io()
+    pass
 
 
 
